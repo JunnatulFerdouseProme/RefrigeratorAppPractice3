4f4fb684cd3dcdfdb97d7445ca304fb0b6e817ab
junnatul.ferdouse@gmail.com <junnatul.ferdouse@gmail.com>
2019-10-16 12:49:09 +0600

    Add project files.

---
+++ RefrigeratorAppPractice3/Refigerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refigerator
+    {
+        
+        public double maxweight { set; get; }
+        public double noOfItems { set; get; }
+        public double unitOfWeight { set; get; }
+        //public List<double> maxweight = new List<double>();
+        //public List<double> noOfItems = new List<double>();
+        //public List<double> unitOfWeight = new List<double>();
+        //public List<double> remainingWeight = new List<double>();
+
+        public double GetRemainingWeight()
+        {
+            double remainingWeight = maxweight - (noOfItems * unitOfWeight);
+            return remainingWeight;
+        }
+        public double GetCurrentWeight()
+
+        {
+            double currentWeight = noOfItems * unitOfWeight;
+            return currentWeight;
+
+        }
+    }
+   
+   
+    
+}
+      
+    
+
---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+        Refigerator refigerator=new Refigerator();
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            refigerator.maxweight = Convert.ToDouble(maxWeightTakeTextBox.Text);
+
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                refigerator.noOfItems = Convert.ToDouble(itemTextBox.Text);
+                refigerator.unitOfWeight = Convert.ToDouble(weightTextBox.Text);
+
+
+            double currentWeight = refigerator.GetCurrentWeight();
+            double remainingWeight = refigerator.GetRemainingWeight();
+                if (currentWeight < refigerator.maxweight)
+                {
+                    currentWeightTextBox.Text = currentWeight.ToString();
+                    remainingWeightTextBox.Text = remainingWeight.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Your given weight can't be entered because it will be overflowen");
+                }
+               
+
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+    }
+}
